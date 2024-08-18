72f9c0f38316acfac2efa70502d512f34ba21844
Kanha Namdev <147715645+knamdev-git@users.noreply.github.com>
2024-08-18 22:10:39 +0530

    Add files via upload

---
+++ IO Statements/bufferreader_class_in_java.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class bufferreader_class_in_java {
+  
+  public static void main(String[] args) throws Exception 
+  {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.println("Enter the character ");
+    int ch = bf.read();
+    System.out.println("ASCII value : "+ch);
+    System.out.println("Character value : "+(char)ch);
+
+
+  }
+
+  
+
+  // OUTPUT
+  /*-->  javac bufferreader_class_in_java.java
+
+    -->  java bufferreader_class_in_java       
+          Enter the character 
+          a
+         ASCII value :  97
+         Character value : a 
+
+
+
+    # Then we should have to use Typecasting for not changing the ASCII Value
+  */
+}
