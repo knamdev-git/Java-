d65328e00054d85b6294ecf3ed7fb9554354a94e
knamdev-git <kanhanamdev011@gmail.com>
2025-07-01 22:46:08 +0530

    LambdaFeature Class Added

---
+++ Array/LambdaFeature.java
@@ -0,0 +1,24 @@
+package com.java8features;
+
+interface Iname{
+	void method1();
+}
+
+//inside of creating the class with implemented method  
+class IClass implements Iname{
+	public void method1() {
+		System.out.println("Iclass method");
+	}
+}
+
+public class LambdaFeature {
+	public static void main(String[] args) {		
+		//and create the obj then call method of interface through class 
+		IClass ob = new IClass();
+		ob.method1();
+		
+		//we use lamba expression for functional methods -> has only one method inside an interface
+		Iname obj = ()->{ System.out.println("This is only the method inside Iname"); };
+		obj.method1();
+	}
+}
